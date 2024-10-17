dae92c459fe3d2b605082b92901bedf4234c290d
Tobias Schipper <tobias.schipper@student.nhlstenden.com>
2024-10-17 18:18:29 +0200

    Verder gegaan met Oefentoets

--- OefenToets/index.php
+++ OefenToets/index.php
@@ -1,3 +1,11 @@
+<?php 
+
+    if($_SERVER["REQUEST_METHOD"] == "POST")
+    { 
+        $email = filter_input(INPUT_POST, "email", FILTER_VALIDATE_EMAIL);
+        $Wachtw = filter_input(INPUT_POST, "WachtW");
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,42 +19,70 @@
     <div class="Header">
         <img src="./img/logo_geekaboo.png" alt="Logo-Geekaboo">
     </div>
+    <div class="Menu">
+        <a>CONTACT</a>
+        <a>REGISTER</a>
+        <a>HOMEPAGE</a>
+    </div>
     <div class="Content">
         <div class="login">
             <h2>Login</h2>
+            <form action="<?php echo $_SERVER["PHP_SELF"] ?>" method="POST">
+            <div>
+                <label for="email">Email:</label>
+                <input type="text" name="email" id="email">
+                <label for="vNaam">Password:</label>
+                <input type="text" name="Wachtwoord" id="WachtW">
+                <div>
+                    <label for="">User:</label>
+                    <input type="radio" name="aantal" value="user" id="user">
+                    <label for="">Admin:</label>
+                    <input type="radio" name="aantal" value="admin" id="admin">
+                </div>
+            </div>
+            <div>
+                <input type="submit" value="Login">
+            </div>
+            </form>
         </div>
         <div class="centerContent">
             <h2>Browsers</h2>
         </div>
         <div class="welcome">
             <h2>Welcome to GeekaBoo</h2>
+            <p>
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui.
+            </p>
         </div>
     </div>
     <div class="Footer">
         <div class="info">
-            <h3></h3>
+            <h3>Info about Geekaboo</h3>
             <p>
-                Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, quis gravida magna mi a libero. Fusce vulputate eleifend sapien. Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus. Nullam accumsan lorem in dui. Cras ultricies mi eu turpis hendrerit fringilla. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In ac dui quis mi consectetuer lacinia. 
+                Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum.  
             </p>
         </div>
         <div class="service">
-            <h3></h3>
+            <h3>Service at Geekaboo</h3>
             <p>
-                Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, quis gravida magna mi a libero. Fusce vulputate eleifend sapien. Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus. Nullam accumsan lorem in dui. Cras ultricies mi eu turpis hendrerit fringilla. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In ac dui quis mi consectetuer lacinia. 
+                Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. 
             </p>
         </div>
         <div class="more">
-            <h3></h3>
+            <h3>More from Geekaboo</h3>
             <p>
-                Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, quis gravida magna mi a libero. Fusce vulputate eleifend sapien. Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus. Nullam accumsan lorem in dui. Cras ultricies mi eu turpis hendrerit fringilla. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In ac dui quis mi consectetuer lacinia. 
+                Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. 
             </p>
         </div>
         <div class="warranty">
-            <h3></h3>
+            <h3>Warranty at Geelaboo</h3>
             <p>
-                Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, quis gravida magna mi a libero. Fusce vulputate eleifend sapien. Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus. Nullam accumsan lorem in dui. Cras ultricies mi eu turpis hendrerit fringilla. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In ac dui quis mi consectetuer lacinia. 
+                Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. 
             </p>
         </div>
+    </div>
+    <div class="copyRight">
+        <p>© GeekaBoo 2024</p>
     </div>
 </body>
 </html>
